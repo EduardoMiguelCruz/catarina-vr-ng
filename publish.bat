c5db728d4b4728033de3b6fb87eaedd5f6d69da6
Eduardo Miguel <eduardomiguel.2109@gmail.com>
2022-07-04 16:30:30 +0100

    banner fix

--- publish.bat
+++ publish.bat
@@ -3,5 +3,4 @@ cd %~dp0
 @REM npm install -g angular-cli-ghpages
 ng build --configuration production --base-href https://EduardoMiguelCruz.github.io/catarina-vr-ng/
 ngh --dir=dist/catarina-vr-ng
-git checkout main
 pause
