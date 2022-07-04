6127c53d05d419093ac1a8705331f4117da5cd90
Eduardo Miguel <eduardomiguel.2109@gmail.com>
2022-07-04 16:23:12 +0100

    Merge branch 'master' of https://github.com/EduardoMiguelCruz/catarina-ar into angular

--- publish.bat
+++ publish.bat
@@ -1,8 +1,7 @@
 @ECHO off
 cd %~dp0
-git checkout -b gh-pages
-git push origin gh-pages
-npm install -g angular-cli-ghpages
-ng build --configuration production --base-href https://EduardoMiguelCruz.github.io/catarina-ar-ng/
-ngh --dir=dist/catarina-vr-angular
+@REM npm install -g angular-cli-ghpages
+ng build --configuration production --base-href https://EduardoMiguelCruz.github.io/catarina-vr-ng/
+ngh --dir=dist/catarina-vr-ng
+git checkout main
 pause
